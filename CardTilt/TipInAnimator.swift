4e6373cf231194bcd518651831954b9796113d61
Ray Fix <rayfix@gmail.com>
2014-08-06 19:33:15 +0900

    Real tip in animation.

--- CardTilt/TipInAnimator.swift
+++ CardTilt/TipInAnimator.swift
@@ -7,13 +7,26 @@
 //
 
 import UIKit
+import QuartzCore // 1
 
 class TipInCellAnimator {
-    // placeholder for things to come -- only fades in for now
     class func animate(cell:UITableViewCell) {
         if let view = cell.contentView {
-            view.layer.opacity = 0.1
-            UIView.animateWithDuration(1.4) {
+            let rotationDegrees: CGFloat = -15.0
+            let rotationRadians: CGFloat = rotationDegrees * (CGFloat(M_PI)/180.0)
+            let offset = CGPointMake(-20, -20)
+            var startTransform = CATransform3DIdentity // 2
+            startTransform = CATransform3DRotate(CATransform3DIdentity,
+                rotationRadians, 0.0, 0.0, 1.0) // 3
+            startTransform = CATransform3DTranslate(startTransform, offset.x, offset.y, 0.0) // 4
+            
+            // 5
+            view.layer.transform = startTransform
+            view.layer.opacity = 0.8
+            
+            // 6
+            UIView.animateWithDuration(0.4) {
+                view.layer.transform = CATransform3DIdentity
                 view.layer.opacity = 1
             }
         }
