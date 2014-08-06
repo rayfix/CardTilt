c9b28ff3f8e945f6930ab9dac1875cd06ce56aa5
Ray Fix <rayfix@gmail.com>
2014-08-06 19:34:21 +0900

    Refactored tip in animation.

--- CardTilt/TipInAnimator.swift
+++ CardTilt/TipInAnimator.swift
@@ -7,24 +7,27 @@
 //
 
 import UIKit
-import QuartzCore // 1
+import QuartzCore
+
+let TipInCellAnimatorStartTransform:CATransform3D = {
+    let rotationDegrees: CGFloat = -15.0
+    let rotationRadians: CGFloat = rotationDegrees * (CGFloat(M_PI)/180.0)
+    let offset = CGPointMake(-20, -20)
+    var startTransform = CATransform3DIdentity
+    startTransform = CATransform3DRotate(CATransform3DIdentity,
+        rotationRadians, 0.0, 0.0, 1.0)
+    startTransform = CATransform3DTranslate(startTransform, offset.x, offset.y, 0.0)
+    
+    return startTransform
+    }()
 
 class TipInCellAnimator {
     class func animate(cell:UITableViewCell) {
         if let view = cell.contentView {
-            let rotationDegrees: CGFloat = -15.0
-            let rotationRadians: CGFloat = rotationDegrees * (CGFloat(M_PI)/180.0)
-            let offset = CGPointMake(-20, -20)
-            var startTransform = CATransform3DIdentity // 2
-            startTransform = CATransform3DRotate(CATransform3DIdentity,
-                rotationRadians, 0.0, 0.0, 1.0) // 3
-            startTransform = CATransform3DTranslate(startTransform, offset.x, offset.y, 0.0) // 4
             
-            // 5
-            view.layer.transform = startTransform
+            view.layer.transform = TipInCellAnimatorStartTransform
             view.layer.opacity = 0.8
             
-            // 6
             UIView.animateWithDuration(0.4) {
                 view.layer.transform = CATransform3DIdentity
                 view.layer.opacity = 1
